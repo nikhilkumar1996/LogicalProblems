3c6ed8c75659f8be25a5ecf90003df6ed05e94fe
nikhilkumar1996 <nikhilkumar1097@gmail.com>
2022-02-02 04:58:29 +0530

    [Nikhil Kumar]:Welcome to Logical Programs

---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace LogicalPrograms
+{
+    public class Program
+    {
+
+       public  static void Main()
+        {
+            Console.WriteLine("Welcome to Logical Programs");
+            return;
+        }
+        
+    }
+}
