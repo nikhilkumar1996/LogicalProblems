e65334ce1a37f6d8fdd26599a78c629503e1fea7
nikhilkumar1996 <nikhilkumar1097@gmail.com>
2022-02-02 05:39:39 +0530

    [Nikhil Kumar]:FibonacciSeries Program Added

---
+++ LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class FibonacciSeries
+    {
+        public void Sum(int number)
+        {
+            int firstNumber = 0, secondNumber = 1, thirdNumber = 0;
+            Console.WriteLine("FibonacciSeries:");
+            Console.WriteLine(firstNumber +"\n"+ secondNumber +" ");
+            for (int i = 0; i <= number; i++)
+            {
+
+                thirdNumber = firstNumber + secondNumber;
+                Console.WriteLine(thirdNumber +" ");
+                firstNumber = secondNumber;
+                secondNumber = thirdNumber;
+            }
+            
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -3,11 +3,32 @@ namespace LogicalPrograms
 {
     public class Program
     {
-
+       
        public  static void Main()
         {
             Console.WriteLine("Welcome to Logical Programs");
-            return;
+            Console.WriteLine("\n 1. Fibonacci Series \n" +
+                              "\n 2. Exit \n ");
+            Console.WriteLine("Choose Progrmas");
+            int n = Convert.ToInt32(Console.ReadLine());
+            switch (n)
+            {
+                case 1:
+                    Console.WriteLine("Enter a Number");
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    FibonacciSeries series = new FibonacciSeries();
+                    series.Sum(num);
+                    break;
+
+                case 2:
+                    Console.WriteLine("Exit");
+                    break;
+
+                default:
+                    Console.WriteLine("Enter Correct Options");
+                    break;
+            }
+            
         }
         
     }
