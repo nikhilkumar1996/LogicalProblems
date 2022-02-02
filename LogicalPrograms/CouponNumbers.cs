248f601a2edb00c0c5a47d818bb8c53573a945da
nikhilkumar1996 <nikhilkumar1097@gmail.com>
2022-02-02 21:24:30 +0530

    [Nikhil Kumar]:CouponNumbers Program Added

---
+++ LogicalPrograms/CouponNumbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class CouponNumbers
+    {
+        static Random random = new Random();
+        public void RandomNumber()
+        {
+            int couponStart, couponEnd;
+            Console.WriteLine("Enter start range of coupon number");
+            couponStart = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter End range of coupon number");
+            couponEnd = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+            int randomNumber = random.Next(couponStart, couponEnd);
+            List<int> number = new List<int>();
+            number.Add(randomNumber);
+            List<int> distinct = number.Distinct().ToList();
+            foreach (int i in distinct)
+            {
+                Console.WriteLine(" Distinct coupon nummber is :" + i);
+            }
+
+
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -3,15 +3,16 @@ namespace LogicalPrograms
 {
     public class Program
     {
-       
-       public  static void Main()
+
+        public static void Main()
         {
             Console.WriteLine("Welcome to Logical Programs");
             Console.WriteLine("\n 1. Fibonacci Series \n" +
                               "\n 2. Perfect Numbers \n" +
                               "\n 3. Prime Number   \n" +
                               "\n 4. Reverse Number \n" +
-                              "\n 5. Exit \n ");
+                              "\n 5. Coupon Number  \n" +
+                              "\n 6. Exit \n ");
             Console.WriteLine("Choose Progrmas");
             int n = Convert.ToInt32(Console.ReadLine());
             switch (n)
@@ -24,31 +25,39 @@ namespace LogicalPrograms
                     break;
 
                 case 2:
-                    PerfectNumber PN =new PerfectNumber();
+                    PerfectNumber PN = new PerfectNumber();
                     PN.Remainder();
                     break;
 
                 case 3:
-                    PrimeNumber prime=new PrimeNumber();
+                    PrimeNumber prime = new PrimeNumber();
                     prime.Number();
                     break;
 
                 case 4:
-                    ReverseNumber reverse=new ReverseNumber();
+                    ReverseNumber reverse = new ReverseNumber();
                     reverse.Number();
                     break;
 
-
                 case 5:
+                    
+                    CouponNumbers CN=new CouponNumbers();
+                    CN.RandomNumber();
+
+                    break;
+
+                case 6:
                     Console.WriteLine("Exit");
                     break;
+
+
 
                 default:
                     Console.WriteLine("Enter Correct Options");
                     break;
             }
-            
         }
-        
+
     }
+        
 }
